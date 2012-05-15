af6c08a9349c131fc6d8a10bc7d37664edcfe3af
Anton <antonjyha@gmail.com>
2012-05-15 02:34:42 +0200

    Fix: correct shifting between directories with cwd,cdup commands Add commands: pasv, port, retr, dele, rmd, stor Current problems and tasks: 1)Files are damaged in fact of transferring between ftp and local machine 2)Make donwload\upload resume 3)Make authentification check before serious commands

--- FtpServer/test/ftp_server.rb
+++ FtpServer/test/ftp_server.rb
@@ -11,14 +11,19 @@ class FTPServer
     @server = TCPServer.new(@config[:host], @config[:port])
    end
 
-   def log
-    return @config[:logger] if @config[:logger]
-    return EmptyLogger.new
+   class DummyLogger
+    def method_missing(method_name, *args, &block); end
    end
 
-   class EmptyLogger
-    def method_missing(method_name, *args, &block); end
-  end
+   # Returns logger
+   def log
+    return @config[:logger] if @config[:logger]
+    return DummyLogger.new
+   end
+
+   def not_implemented
+    status(500);
+   end
 
    def run
     threads = []
@@ -51,7 +56,7 @@ class FTPServer
       command.downcase! if command
       m = 'cmd_'+command.to_s
       if self.respond_to?(m, true)
-        puts m;
+        puts m
         self.send(m, params[1])
       else
         status(500);
@@ -97,6 +102,72 @@ class FTPServer
     else
       status(code, '')
     end
+   end
+
+   def data_connection(&block)
+    client_socket = nil
+    if (thread[:passive])
+      unless (IO.select([thread[:data_socket]], nil, nil, 60000))
+        status 425
+        return false
+      end
+      client_socket = thread[:data_socket].accept
+      status 150
+    else
+      client_socket = thread[:data_socket]
+      status 125
+    end
+    yield(client_socket)
+    return true
+  ensure
+    client_socket.close if client_socket && thread[:passive]
+    client_socket = nil
+   end
+
+   def open_object(path)
+    if (path[0,1] == '/') || (path.is_a?(Array) && (path[0] == ''))
+      dir = @config[:root]
+    else
+      dir = thread[:cwd]
+    end
+    path = path.split('/') unless path.is_a?(Array)
+    return dir if path.empty?
+    last_element = path.pop
+    path.each do |p|
+      unless p == ''
+        dir = dir.ftp_list.detect {|d| (d.ftp_name.casecmp(p) == 0) && (d.ftp_dir) }
+        return nil unless dir
+      end
+    end
+    dir = dir.ftp_list.detect {|d| (d.ftp_name.casecmp(last_element) == 0) } unless last_element == ''
+    return dir
+  end
+
+  def open_path(path)
+    result = open_object(path)
+    result = nil if result && !result.ftp_dir
+    return result
+  end
+
+  def open_file(path)
+    result = open_object(path)
+    result = nil if result && result.ftp_dir
+    return result
+  end
+
+  def get_path(object)
+    return '/' unless object
+    return '/' if object == @config[:root]
+    result = ''
+    while object do
+      result = object.ftp_name + '/' + result
+      object = object.ftp_parent
+    end
+    return result
+  end
+
+  def get_quoted_path(object)
+    get_path(object).gsub('"', '""')
   end
 
   #commands
@@ -125,33 +196,66 @@ class FTPServer
     status 257, "\"#{ get_path(thread[:cwd]).gsub('"', '""') }\" is the current directory"
   end
 
-   def get_path(object)
-    return '/' unless object
-    return '/' if object == @config[:root]
-    result = ''
-    while object do
-      result = '/' + object.ftp_name + result
-      object = object.ftp_parent
-    end
-    return result
-  end
-
-  def get_quoted_path(object)
-    get_path(object).gsub('"', '""')
-  end
-
   def cmd_cdup(params)
     thread[:cwd] = thread[:cwd].ftp_parent
     thread[:cwd] = @config[:root] unless thread[:cwd]
     status(250, 'Directory successfully changed.')
   end
 
+  def cmd_dele(path)
+    if (file = open_file(path)) && file.ftp_delete(false)
+      status 250
+    else
+      status(550, 'Delete operation failed.')
+    end
+  end
+
+  def cmd_feat(params)
+    thread[:socket].puts "211-Features\r\n"
+    thread[:socket].puts " UTF8\r\n"
+    thread[:socket].puts "211 end\r\n"
+  end
+
   def cmd_list(params)
-    list = thread[:cwd].ftp_list
-    list.each {|file| thread[:socket].puts(file.ftp_name + "\r\n") }
+    data_connection do |data_socket|
+      list = thread[:cwd].ftp_list
+      list.each {|file| data_socket.puts((file.ftp_dir ? 'd': '-') + 'rw-rw-rw- 1 ftp ftp ' + file.ftp_size.to_s + ' ' + file.ftp_date.strftime('%b %d %H:%M') + ' ' + file.ftp_name + "\r\n") }
+    end
+    thread[:data_socket].close if thread[:data_socket]
+    thread[:data_socket] = nil
+
+    status 226, "Transfer complete"
+  end
+
+  def cmd_mdtm(path)
+    if (file = open_file(path))
+      status 213, file.ftp_date.strftime('%Y%m%d%H%M%S')
+    else
+      status(550, 'Could not get modification time.')
+    end
+  end
+
+  def cmd_mkd(path)
+    dir = open_object(path)
+    if (dir)
+      status 521, "Directory already exists"
+      return
+    end
+    splitted_path = path.split('/')
+    mkdir = splitted_path.pop
+    dir = open_path(splitted_path)
+    if dir && (newone = dir.ftp_create(mkdir, true))
+      status 257, "\"#{ get_path(thread[:cwd]).gsub('"', '""') +path }\" directory created."            #KOKO"\"#{ get_path(thread[:cwd]).gsub('"', '""') }\"
+    else
+      status 550
+    end
   end
 
   def cmd_cwd(path)
+    if path == '' || !path
+      status(550, 'Failed to change directory.')
+      return
+    end
     if (newpath = open_path(path))
       thread[:cwd] = newpath
       status(250, 'Directory successfully changed.')
@@ -160,29 +264,97 @@ class FTPServer
     end
   end
 
-  def open_path(path)
-    result = open_object(path)
-    result = nil if result && !result.ftp_dir
-    return result
-  end
-
-  def open_object(path)
-    if (path[0,1] == '/') || (path.is_a?(Array) && (path[0] == ''))
-      dir = @config[:root]
+  def cmd_rmd(path)
+    if (dir = open_path(path)) && dir.ftp_delete(true)
+      status 250
     else
-      dir = thread[:cwd]
+      status(550, 'Remove directory operation failed.')
     end
-    path = path.split('/') unless path.is_a?(Array)
-    return dir if path.empty?
-    last_element = path.pop
-    path.each do |p|
-      unless p == ''
-        dir = dir.ftp_list.detect {|d| (d.ftp_name.casecmp(p) == 0) && (d.ftp_dir) }
-        return nil unless dir
-      end
-    end
-    dir = dir.ftp_list.detect {|d| (d.ftp_name.casecmp(last_element) == 0) } unless last_element == ''
-    return dir
   end
 
+ def cmd_port(ip_port)
+    s = ip_port.split(',')
+    port = s[4].to_i * 256 + s[5].to_i
+    host = s[0..3].join('.')
+    if thread[:data_socket]
+      thread[:data_socket].close
+      thread[:data_socket] = nil
+    end
+    thread[:data_socket] = TCPSocket.new(host, port)
+    thread[:passive] = false
+    status 200, "Passive connection established (#{port})"
+  end
+
+  def cmd_pasv(params)
+    if thread[:data_socket]
+      thread[:data_socket].close
+      thread[:data_socket] = nil
+    end
+    thread[:data_socket] = TCPServer.new('localhost', 0)
+    thread[:passive] = true
+    port = thread[:data_socket].addr[1]
+    port_lo = port & "0x00FF".hex
+    port_hi = port >> 8
+    ip = thread[:data_socket].addr[3]
+    ip = ip.split('.')
+    status 227, "Entering Passive Mode (#{ip[0]},#{ip[1]},#{ip[2]},#{ip[3]},#{port_hi},#{port_lo})"
+  end
+
+  def cmd_retr(path)
+    if (file = open_file(path))
+      data_connection do |data_socket|
+        if file.ftp_retrieve(data_socket)
+          status 226, 'Transfer complete'
+        else
+          status(550, 'Failed to open file.')
+        end
+      end
+    else
+      status(550, 'Failed to open file.')
+    end
+
+    thread[:data_socket].close if thread[:data_socket]
+    thread[:data_socket] = nil
+  end
+
+  def cmd_size(path)
+    if (file = open_file(path))
+      status 213, file.ftp_size.to_s
+    else
+      status(550, 'Could not get file size.')
+    end
+  end
+
+  def cmd_stor(path)
+    file = open_file(path)
+    if file
+      status 553, 'Could not create file.'
+      return
+    end
+    unless file
+      splitted_path = path.split('/')
+      filename = splitted_path.pop
+      dir = open_path(splitted_path)
+      file = dir.ftp_create(filename) if dir
+    end
+    if file
+      data_connection do |data_socket|
+        file.ftp_store(data_socket)
+      end
+      status 226, 'Transfer complete'
+    else
+      status 550, 'Failed to open file.'
+    end
+
+    thread[:data_socket].close if thread[:data_socket]
+    thread[:data_socket] = nil
+  end
+
+  def cmd_syst(params)
+    status(215, RUBY_PLATFORM)
+  end
+
+  def cmd_type(type)
+    status 200, "Type set."
+  end
 end
--- FtpServer/test/vfs.rb
+++ FtpServer/test/vfs.rb
@@ -4,7 +4,9 @@ class FileSystem
     def ftp_list
       output = Array.new
       Dir.entries(@path).sort.each do |file|
-        output << FileSystem.new(@path + '/' + file)
+        if(file!='')
+          output << FileSystem.new(@path + '/' + file, self)
+        end
       end
       return output
     end
@@ -20,14 +22,11 @@ class FileSystem
       else
         FileSystem.new(@path + '/' + name, self)
       end
-
     end
 
     def ftp_retrieve(output)
       output << File.new(@path, 'r').read
     end
-
-    #def ftp_parent(
 
     def ftp_store(input)
       return false unless File.open(@path, 'w') do |f|
@@ -37,7 +36,20 @@ class FileSystem
       @ftp_date = File.mtime(@path) if File.exists?(@path)
     end
 
-    def delete()
+    def ftp_delete(dir = false)
+      if dir
+        begin
+        if Dir.rmdir(@path)
+          return true
+        end
+        rescue SystemCallError
+          return false
+          end
+      else
+        if File.delete(@path)
+          return true
+        end
+     end
       return false
     end
 
@@ -46,11 +58,11 @@ class FileSystem
       @ftp_parent = parent
       @ftp_name = path.split('/').last
       @ftp_name = '/' unless @ftp_name
+      @ftp_name = '' unless path.split('/').length>2
       @ftp_dir = File.directory?(path)
       @ftp_size = File.size?(path)
       @ftp_size = 0 unless @ftp_size
       @ftp_date = Time.now
       @ftp_date = File.mtime(path) if File.exists?(path)
     end
-
 end
